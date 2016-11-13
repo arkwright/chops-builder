90a3bee1181f891d2b5020ebc6e2f211699617c3
Robert Arkwright <rarkwright@gmail.com>
2016-11-13 15:55:55 -0500

    Install Webpack.

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  entry: __dirname + '/app/entry.js',
+  output: {
+    path: __dirname + '/build',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+    ]
+  }
+};
